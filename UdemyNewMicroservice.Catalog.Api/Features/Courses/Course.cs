b1680766f10b87e123a50f57957ddfacdfb35613
Mert Yılmaz <mertyllmz1@gmail.com>
2025-08-28 14:58:45 +0300

    Created Category and Course classes. BaseEntity for _Id property
    Add catalog API entities and MongoDB integration
    
    - Introduced `Category`, `Course`, and `Feature` classes to represent catalog entities.
    - Added `BaseEntity` class with BSON serialization for MongoDB identifiers.
    - Updated project file to include `MongoDB.EntityFrameworkCore` and `NewId` package references.
    - Removed unnecessary folder reference in the project file.

---
+++ UdemyNewMicroservice.Catalog.Api/Features/Courses/Course.cs
@@ -0,0 +1,21 @@
+﻿using UdemyNewMicroservice.Catalog.Api.Features.Categories;
+using UdemyNewMicroservice.Catalog.Api.Repositories;
+
+namespace UdemyNewMicroservice.Catalog.Api.Features.Courses
+{
+	public class Course:BaseEntity
+	{
+		/* 
+		 * default! -> null gelmeyecek demek.
+	  */
+		public string Name { get; set; }= default!;
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		public Guid UserId{ get; set; }
+		public string Picture { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public Guid CategoryId { get; set; }
+		public Category Category { get; set; } = default!; 
+		public Feature Feature { get; set; } = default!;
+	}
+}
---
+++ UdemyNewMicroservice.Catalog.Api/Repositories/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace UdemyNewMicroservice.Catalog.Api.Repositories
+{
+	public class BaseEntity
+	{
+		/* #NOTES
+		 * snow flake algoritması ile id oluşturulabilir. Indexlenmesi daha hızlı olur.		
+		 * Dolayısıyla Guidleri kendimiz oluşturabiliriz. NewId paketi ile indexlenebilir Guidler oluşturulabilir.			
+		 */
+
+		[BsonElement("_id")]
+		public Guid Id { get; set; }
+	}
+}
